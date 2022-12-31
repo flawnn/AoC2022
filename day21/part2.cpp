daa7d2225c930eabd20cb75ba68dd1c013c9d29c
Flawn <fl4wn@protonmail.com>
2022-12-31 18:58:55 +0100

    :heavy_plus_sign: Add day 21

---
+++ day21/part2.cpp
@@ -0,0 +1,260 @@
+/* Optimization Stuff */
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+/********   All Required Header Files ********/
+#include <bits/stdc++.h>
+#include <ranges>
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+/**********************************************/
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+  int i = 20;
+  char buf[21];
+  // buf[10] = 0;
+  buf[20] = '\n';
+
+  do
+  {
+    buf[--i] = x % 10 + '0';
+    x/= 10;
+  }while(x);
+  do
+  {
+    putchar(buf[i]);
+  } while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+  T n=0,s=1;
+  char p=getchar();
+  if(p=='-')
+    s=-1;
+  while((p<'0'||p>'9')&&p!=EOF&&p!='-')
+    p=getchar();
+  if(p=='-')
+    s=-1,p=getchar();
+  while(p>='0'&&p<='9') {
+    n = (n<< 3) + (n<< 1) + (p - '0');
+    p=getchar();
+  }
+
+  return n*s;
+}
+
+
+vector<string> tokenize(std::string const &str, const char delim)
+{
+    // construct a stream from the string
+    std::stringstream ss(str);
+
+  vector<string> command;
+ 
+    std::string s;
+  
+    while (std::getline(ss, s, delim)) {
+        command.push_back(s);
+    }
+
+  return command;
+}
+
+template<typename T>
+vector<int> getKeys(map<int, T> &items){
+  std::vector<int> itemKeys;
+
+  for (const auto& [key, _] : items) {
+      itemKeys.push_back(key);
+  }
+
+  return itemKeys;
+}
+
+template <typename t> void move(std::vector<t>& v, size_t oldIndex, size_t newIndex)
+{
+    if (oldIndex > newIndex)
+        std::rotate(v.rend() - oldIndex - 1, v.rend() - oldIndex, v.rend() - newIndex);
+    else        
+        std::rotate(v.begin() + oldIndex, v.begin() + oldIndex + 1, v.begin() + newIndex + 1);
+}
+vector<pair<string, vector<string>>> jobs;
+
+vector<pair<string, vector<string>>> getRelevantCalcys(pair<string, vector<string>> monkey){
+    vector<pair<string, vector<string>>> relevantCalcys {monkey};
+    if(monkey.second.size() == 3){
+        if(monkey.second[0] == "humn"){
+            pair<string, vector<string>> operand2 = *find_if(jobs.begin(), jobs.end(), [&monkey](auto job){
+              return job.first == monkey.second[2];
+            });
+
+            vector<pair<string, vector<string>>> res2 = getRelevantCalcys(operand2);
+            relevantCalcys[0].second[2] = res2[0].second[0];
+
+            return relevantCalcys;
+        } else if(monkey.second[2] == "humn"){
+            pair<string, vector<string>> operand2 = *find_if(jobs.begin(), jobs.end(), [&monkey](auto job){
+              return job.first == monkey.second[0];
+            });
+
+            vector<pair<string, vector<string>>> res1 = getRelevantCalcys(operand2);
+            relevantCalcys[0].second[0] = res1[0].second[0];
+            
+            return relevantCalcys;
+        }
+
+        pair<string, vector<string>> operand1 = *find_if(jobs.begin(), jobs.end(), [&monkey](auto job){
+            return job.first == monkey.second[0];
+        });
+
+        pair<string, vector<string>> operand2 = *find_if(jobs.begin(), jobs.end(), [&monkey](auto job){
+            return job.first == monkey.second[2];
+        });
+
+        vector<pair<string, vector<string>>> res1 = getRelevantCalcys(operand1);
+        vector<pair<string, vector<string>>> res2 = getRelevantCalcys(operand2);
+
+        if(res1[0].first == "" && res2[0].first != "" ){
+            relevantCalcys[0].second[0] = res1[0].second[0];
+            relevantCalcys.insert(relevantCalcys.end(), res2.begin(), res2.end());
+        } else if(res1[0].first != "" && res2[0].first == "" ){
+            relevantCalcys[0].second[2] = res2[0].second[0];
+            relevantCalcys.insert(relevantCalcys.end(), res1.begin(), res1.end());
+        } else {
+          long long res = 0;
+          long long numm1 = stoll(res1[0].second[0]);
+          long long numm2 = stoll(res2[0].second[0]);
+
+          if(monkey.second[1] == "+"){
+            res = numm1 + numm2;
+          } else if(monkey.second[1] == "-"){
+            res = numm1 - numm2;
+          } else if(monkey.second[1] == "*"){
+            res = numm1 * numm2;
+          } else if(monkey.second[1] == "/"){
+            res = numm1 / numm2;
+          }
+          
+          return {{"", {(to_string(res))}}};
+        }
+
+        return relevantCalcys;
+    } else {
+        return {{"", {(monkey.second[0])}}};
+    }
+}
+
+
+long long part2(pair<string, vector<string>> monkey){
+    vector<pair<string, vector<string>>> relevantCalcys = getRelevantCalcys(monkey);
+    vector<pair<string, string>> ops;
+
+    long long num = stoll(isdigit(relevantCalcys[0].second[0][0]) ? relevantCalcys[0].second[0] : relevantCalcys[0].second[2]);
+    relevantCalcys.erase(relevantCalcys.begin());
+
+    for(auto &step: relevantCalcys){
+      if(isdigit(step.second[0][0])){
+        if(step.second[1] == "+" || step.second[1] == "*"){
+          if(step.second[1] == "+"){
+            num -= stoll(step.second[0]);
+          } else {
+            num /= stoll(step.second[0]);
+          }
+        } else {
+          if(step.second[1] == "-"){
+            num -= stoll(step.second[0]);
+            num *= -1;
+          } else {
+            num /= stoll(step.second[0]);
+            // take power of -1 of num
+            num = pow(num, -1);
+          }
+        }
+      } else {
+        if(step.second[1] == "+" || step.second[1] == "*"){
+          if(step.second[1] == "+"){
+            num -= stoll(step.second[2]);
+          } else {
+            num /= stoll(step.second[2]);
+          }
+        } else {
+          if(step.second[1] == "-"){
+            num += stoll(step.second[2]);
+          } else {
+            num *= stoll(step.second[2]);
+          }
+      }
+    }    
+}
+return num;
+}
+
+
+int main()
+{
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  #endif
+  string input = "";
+  vector<int> nums;
+  while(getline(cin, input)){
+    auto split = tokenize(input, ':');
+    auto job = tokenize(split[1], ' ');
+    if(job.size() == 2){
+      jobs.push_back({split[0], {job[1]}});
+    } else {
+      jobs.push_back({split[0], {job[1], job[2], job[3]}});
+    }
+  }
+
+  auto root = find_if(jobs.begin(), jobs.end(), [](auto job){
+    return job.first == "root";
+  });
+
+  cout << part2(*root) << endl;
+}
